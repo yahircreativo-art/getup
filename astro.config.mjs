631932a869c8ba9b065536db4151a4e93bdcda2d
imeiman <melfer.creativos@gmail.com>
2026-09-01 19:54:05 -0700

    yaml-astro-config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import { siteConfig } from "./src/config/site.config";
 
 export default defineConfig({
+  site: siteConfig.url,
+  base: "/getup",
   vite: {
     plugins: [tailwindcss()],
   },
---
+++ src/config/site.config.js
@@ -0,0 +1,6 @@
+export const siteConfig = {
+  title: 'GetUp',
+  description: 'Tu sitio web de fitness y bienestar',
+  url: 'https://githubusername.github.io/getup',
+  github: 'https://github.com/githubusername/getup',
+};
