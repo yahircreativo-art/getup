720859e446558376d5233b9b4c72faf4d9914734
imeiman <melfer.creativos@gmail.com>
2026-09-04 13:18:42 -0700

    seo-english

--- src/config/site.config.js
+++ src/config/site.config.js
@@ -1,5 +1,17 @@
 export const siteConfig = {
   title: 'GetUp',
   url: 'https://ugetup.com',
-  github: 'https://github.com/yahircreativo-art/getup',
+  description: 'Take your business to the next level with GetUp. We build professional websites for fitness and wellness businesses.',
+  locale: 'en_US',
+  ogImage: '/og-default.png',
+  logo: '/getup-icon.svg',
+  social: {
+    github: 'https://github.com/yahircreativo-art/getup',
+  },
+  business: {
+    type: 'LocalBusiness',
+    addressCountry: 'MX',
+    contactType: 'customer service',
+    languages: ['Spanish', 'English'],
+  },
 };
