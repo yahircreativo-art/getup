724433936df6459185f3c34fd0faec9789e14995
imeiman <melfer.creativos@gmail.com>
2026-09-04 08:48:38 -0700

    site-url

--- src/config/site.config.js
+++ src/config/site.config.js
@@ -1,6 +1,6 @@
 export const siteConfig = {
   title: 'GetUp',
   description: 'Tu sitio web de fitness y bienestar',
-  url: 'https://getup-rose.vercel.app',
+  url: 'https://ugetup.com',
   github: 'https://github.com/yahircreativo-art/getup',
 };
