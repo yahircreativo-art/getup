2f4d26069f6df79a6e79a166d29eda3946272417
imeiman <melfer.creativos@gmail.com>
2026-09-04 09:05:55 -0700

    new-logo

--- src/config/site.config.js
+++ src/config/site.config.js
@@ -1,6 +1,6 @@
 export const siteConfig = {
   title: 'GetUp',
-  description: 'Tu sitio web de fitness y bienestar',
+  description: 'Getup online',
   url: 'https://ugetup.com',
   github: 'https://github.com/yahircreativo-art/getup',
 };
