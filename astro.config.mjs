bb1072b02363761ecdd5b62483917bbe3da57135
imeiman <melfer.creativos@gmail.com>
2026-09-06 11:56:28 -0700

    seo.config.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import { siteConfig } from "./src/config/site.config";
+import { siteConfig } from "./src/config/seo.config";
 
 export default defineConfig({
   site: siteConfig.url,
